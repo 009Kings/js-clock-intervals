7a1b5217b61ed2491269c29f3c9cd589eb209546
009kings <009kings@gmail.com>
2018-12-03 22:02:24 -0800

    wow, such pretty code that will all dissappear to the DRY drought

--- clock.js
+++ clock.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
-	console.log("dom content has loaded");
-	
+    var d = new Date();
+    console.log(d);
+    hrStatus = d.getHours();
+    minStatus = d.getMinutes();
+    secStatus = d.getSeconds();
+    moveSeconds();
+    setInterval(moveSeconds, 1000);
 });
 
 let hrStatus = minStatus = secStatus = 0;
@@ -14,10 +19,30 @@ function moveSeconds () {
         minStatus=0;
         hrStatus += 1; 
     };
-    if (hrStatus >= 12) {
+    if (hrStatus >= 24) {
         hrStatus = 0;
     }
     console.log(`${hrStatus}:${minStatus}:${secStatus}`);
+    minAndSecMovement(secStatus, secondHand);
+    minAndSecMovement(minStatus, minuteHand);
+    hrMove();
 }
 
-setInterval(moveSeconds, 1);
+//Easy access handles
+let secondHand = document.getElementById("second");
+let minuteHand = document.getElementById("minute");
+let hourHand = document.getElementById("hour");
+
+// Determines the state of min/sec and sets arm rotation to match
+function minAndSecMovement(state, element) {
+    var baseIncriment = 6;
+    var currentAngle = state * baseIncriment;
+    element.style.transform = `rotate(${currentAngle}deg)`;
+}
+//minStatus % 12 === 0 +=1 to sth 30d == 1hr rotate.5/min
+function hrMove() {
+    var baseIncriment = .5;
+    var minAngle = minStatus * baseIncriment;
+    var currentAngle = (hrStatus * 30) + minAngle;
+    hourHand.style.transform = `rotate(${currentAngle}deg)`;
+}
