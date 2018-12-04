8968113c63bce1eeb62b3d9c4771ad6a41995510
009kings <009kings@gmail.com>
2018-12-03 22:15:01 -0800

    is it the sahara because this code is DRY
    rather than setting the time once and updating the clock based on interval, it sets the clock every 100 miliseconds based on current time, no need to manually set this clock back for daylight savings time

--- clock.js
+++ clock.js
@@ -1,37 +1,31 @@
 document.addEventListener("DOMContentLoaded", function(){
+    startYerClocks();
+    setInterval(startYerClocks, 100);
+});
+
+//Easy access handles
+let hrStatus = minStatus = secStatus = 0;
+let secondHand = document.getElementById("second");
+let minuteHand = document.getElementById("minute");
+let hourHand = document.getElementById("hour");
+
+function setTime() {
     var d = new Date();
-    console.log(d);
     hrStatus = d.getHours();
     minStatus = d.getMinutes();
     secStatus = d.getSeconds();
-    moveSeconds();
-    setInterval(moveSeconds, 1000);
-});
-
-let hrStatus = minStatus = secStatus = 0;
-function moveSeconds () {
-    secStatus ++;
-    if (secStatus >= 60) {
-        secStatus = 0;
-        minStatus +=1
-    }
-    if (minStatus >= 60) {
-        minStatus=0;
-        hrStatus += 1; 
-    };
-    if (hrStatus >= 24) {
-        hrStatus = 0;
-    }
     console.log(`${hrStatus}:${minStatus}:${secStatus}`);
+}
+function render() {
     minAndSecMovement(secStatus, secondHand);
     minAndSecMovement(minStatus, minuteHand);
     hrMove();
 }
 
-//Easy access handles
-let secondHand = document.getElementById("second");
-let minuteHand = document.getElementById("minute");
-let hourHand = document.getElementById("hour");
+function startYerClocks() {
+    setTime();
+    render();
+}
 
 // Determines the state of min/sec and sets arm rotation to match
 function minAndSecMovement(state, element) {
@@ -39,7 +33,7 @@ function minAndSecMovement(state, element) {
     var currentAngle = state * baseIncriment;
     element.style.transform = `rotate(${currentAngle}deg)`;
 }
-//minStatus % 12 === 0 +=1 to sth 30d == 1hr rotate.5/min
+
 function hrMove() {
     var baseIncriment = .5;
     var minAngle = minStatus * baseIncriment;
